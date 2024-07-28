542171e34353e1c37acc2b32e58efdbc9c4931f1
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-07-28 20:37:23 +0300

    checks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,12 +11,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route("/", strict_slashes = False)
 def hell0_world():
-	"""
-	displays 'Hello HBNB!"
-	"""
-	return "Hello HBNB!"
+    """
+        displays 'Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host = '0.0.0.0', port = 5000)
