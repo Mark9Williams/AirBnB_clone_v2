03e4c45fb882ca4e208ace12674d7204794f4096
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-07-28 11:22:50 +0300

    ident

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,23 +8,23 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""Displays 'Hello HBNB!'"""
-	return "Hello HBNB!"
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""Dispalys 'HBNB'"""
-	return "HBNB"
+    """Dispalys 'HBNB'"""
+    return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text():
-	"""Route to display 'c' followed by value of the text variable"""
-	return "c {}".format(text.replace('_', ' '))
+    """Route to display 'c' followed by value of the text variable"""
+    return "c {}".format(text.replace('_', ' '))
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_text():
-	"""displays 'Python', followed by the value of text"""
-	return "Python {}".format(text.replace('_', ' '))
+    """displays 'Python', followed by the value of text"""
+    return "Python {}".format(text.replace('_', ' '))
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
